e84dbb7fc1f3f2ad83a060b2deb903abca6a858b
Nikita Voino <ankapkekw@gmail.com>
2024-03-25 22:25:37 +0300

    Make full date functions, proverka and e.t.c..

--- Lab2/Task1/mainwindow.h
+++ Lab2/Task1/mainwindow.h
@@ -7,6 +7,9 @@
 #include <QFile>
 #include <QTextStream>
 #include <QDebug>
+#include <QDate>
+#include <QInputDialog>
+#include <QMessageBox>
 #include "mydata.h"
 
 QT_BEGIN_NAMESPACE
@@ -30,11 +33,30 @@ private slots:
 
     void makeTable();
 
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    bool proverka(QString vot);
+
+    void on_pushButton_9_clicked();
+
 private:
     Ui::MainWindow *ui;
     QStandardItemModel *model;
     mydata **moyaDate;
     QString pathFile;
     QString moidates;
+    int kolvo;
 };
 #endif // MAINWINDOW_H
--- Lab2/Task1/mydata.h
+++ Lab2/Task1/mydata.h
@@ -7,18 +7,18 @@ class mydata
 {
 public:
     mydata();
-
     mydata NextDay();
     mydata PreviousDay();
+    bool leaf();
+    short WeekNumber();
     void set(QStandardItemModel *model, QModelIndex &index);
     int DaysTillYourBithday(mydata bithdaydate);
     int Duration (mydata date);
     void get(QString string);
     QString ret();
+
 private:
     QString dates;
-    mydata **moyaDates;
-    int size;
 };
 
 #endif // MYDATA_H
