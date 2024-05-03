4d149c49c947af1e5fc38a0a46b04bef321e85a2
Nikita Voino <ankapkekw@gmail.com>
2024-05-03 09:22:48 +0300

    Task 4 done

--- Lab3/Task4/mainwindow.h
+++ Lab3/Task4/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPointer>
+#include <QMessageBox>
+
+#include "ring.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +23,25 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    Ring** towers[3];
+    int towerSizes[3] = {0, 0, 0};
+    int count = 0;
+    double speed = 1.0;
+
+    QPointer<QPropertyAnimation> animationUp;
+    QPointer<QPropertyAnimation> animationLeftOrRight;
+    QPointer<QPropertyAnimation> animationDown;
+
+
+protected:
+    void paintEvent(QPaintEvent*) override;
+private slots:
+
+    void moveRing(Ring* ring, int x, int y);
+    void hanoi(int n, Ring**& from, Ring**& to, Ring**& aux, int& sizeFrom, int& sizeTo, int& sizeAux, int x1, int x2, int x3);
+
+    void on_pushButton_clicked();
+    void on_spinBox_valueChanged(int n);
+    void on_doubleSpinBox_valueChanged(double n);
 };
 #endif // MAINWINDOW_H
