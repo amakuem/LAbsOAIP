5ffcf7853df43026eb3893d8fa6ee8f43e4b086b
Nikita Voino <ankapkekw@gmail.com>
2024-05-06 02:22:55 +0300

    Make median calculation

--- Lab4/Task3/mainwindow.cpp
+++ Lab4/Task3/mainwindow.cpp
@@ -6,9 +6,65 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //calculateMedians();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::calculateMedians()
+{
+    int j = 0;
+    int sum  = 0;
+    for(int i = 0; i < a.size(); i++)
+    {
+        //count++;
+        if(temp.size() != 3)
+        {
+            temp.push_back(a[i]);
+            j++;
+        }
+        if(temp.size() == 3 || i == a.size() - 1)
+        {
+            j = 0;
+            std::sort(temp.begin(), temp.end());
+
+            if(temp.size() == 3)
+            {
+                b.push_back(temp[1]);
+
+                temp.clear();
+                //temp.push_back(a[i]);
+            }
+            else
+            {
+                for(int m = 0; m < temp.size(); m++)
+                {
+                    sum += temp[m];
+                }
+                b.push_back(sum/temp.size());
+
+                sum = 0;
+                temp.clear();
+                //temp.push_back(a[i]);
+            }
+        }
+    }
+
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    calculateMedians();
+    QString string = "";
+    for (int i = 0; i < b.size(); i++) {
+        string += QString::number(b[i]);
+        string += ", ";
+    }
+    string.chop(2);
+    ui->label->setText(string);
+}
+
--- Lab4/Task3/mainwindow.h
+++ Lab4/Task3/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <algorithm>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +19,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void calculateMedians();
+
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QVector<int> a = {1, 5, 1, 4, 5, 6, 2, 1, 3, 4, 4, 4, 5, 7};
+    QVector<int> temp;
+    QVector<int> b;
+
+
 };
 #endif // MAINWINDOW_H
