095bb31cae740cff03ed1761fec44c053ac07f4e
Nikita Voino <ankapkekw@gmail.com>
2024-05-06 01:02:26 +0300

    Task 2: interpolation sort, binpow, sort speed

--- Lab4/Task1/mainwindow.cpp
+++ Lab4/Task1/mainwindow.cpp
@@ -7,14 +7,17 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     m_data = new int[m_size];
-    tempData = new int[m_size];
+    arrayForTime = new int[m_size];
+
     for (int i = 0; i < m_size; ++i) {
         m_data[i] = rand() % 340;
-        tempData[i] = m_data[i];
+        arrayForTime[i] = m_data[i];
     }
 
     isSorted = false;
     isNumberGood = false;
+
+    ui->label_4->setWordWrap(true);
 
     connect(ui->lineEdit, &QLineEdit::returnPressed, this, &MainWindow::getNumber);
     connect(ui->lineEdit, &QLineEdit::cursorPositionChanged, this, &MainWindow::lineChanged);
@@ -41,49 +44,63 @@ void MainWindow::paintEvent(QPaintEvent*) {
     }
 }
 
+//Task 1
+
 void MainWindow::merge(int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
 
+    // Создаем временные массивы
+    int L[n1], R[n2];
+
     for (i = 0; i < n1; i++)
-        tempData[i] = m_data[l + i];
+        L[i] = m_data[l + i];
     for (j = 0; j < n2; j++)
-        tempData[n1 + j] = m_data[m + 1 + j];
+        R[j] = m_data[m + 1 + j];
 
     i = 0;
     j = 0;
     k = l;
-    while (i < n1 && j < n2) {
-        if (tempData[i] <= tempData[n1 + j]) {
-            m_data[k] = tempData[i];
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            m_data[k] = L[i];
             i++;
-
-            delay(0.5);
-            update();
-        } else {
-            m_data[k] = tempData[n1 + j];
-            j++;
-
-            delay(0.5);
-            update();
         }
+        else
+        {
+            m_data[k] = R[j];
+            j++;
+        }
+
+        delay(50);
+        update();
+
         k++;
     }
 
-    while (i < n1) {
-        m_data[k] = tempData[i];
+    while (i < n1)
+    {
+        m_data[k] = L[i];
         i++;
         k++;
+
+        delay(50);
+        update();
     }
 
-    while (j < n2) {
-        m_data[k] = tempData[n1 + j];
+    while (j < n2)
+    {
+        m_data[k] = R[j];
         j++;
         k++;
-    }
 
+        delay(50);
+        update();
+    }
 }
 
 void MainWindow::mergeSort(int l, int r)
@@ -95,6 +112,71 @@ void MainWindow::mergeSort(int l, int r)
         mergeSort(m + 1, r);
 
         merge(l, m, r);
+    }
+}
+
+void MainWindow::mergeSortTime(int l, int r)
+{
+
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+
+
+        mergeSortTime(l, m);
+        mergeSortTime(m + 1, r);
+
+        mergeTime(l, m, r);
+    }
+}
+
+void MainWindow::mergeTime(int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = m_data[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arrayForTime[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arrayForTime[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arrayForTime[k] = R[j];
+            j++;
+        }
+
+        delay(50);
+        update();
+
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arrayForTime[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arrayForTime[k] = R[j];
+        j++;
+        k++;
     }
 }
 
@@ -119,7 +201,6 @@ void MainWindow::heapify(int n, int i)
 
         heapify(n, largest);
     }
-    update();
 }
 
 void MainWindow::heapSort(int n)
@@ -129,7 +210,40 @@ void MainWindow::heapSort(int n)
 
     for (int i = n - 1; i >= 0; i--) {
         std::swap(m_data[0], m_data[i]);
+        delay(0.5);
+        update();
         heapify(i, 0);
+    }
+}
+
+void MainWindow::heapSortTime(int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapifyTime(n, i);
+
+    for (int i = n - 1; i >= 0; i--) {
+        std::swap(arrayForTime[0], arrayForTime[i]);
+        heapifyTime(i, 0);
+    }
+}
+
+void MainWindow::heapifyTime(int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+
+    if (left < n && arrayForTime[left] > arrayForTime[largest])
+        largest = left;
+
+    if (right < n && arrayForTime[right] > arrayForTime[largest])
+        largest = right;
+
+    if (largest != i) {
+        std::swap(arrayForTime[i], arrayForTime[largest]);
+
+        heapifyTime(n, largest);
     }
 }
 
@@ -165,6 +279,31 @@ void MainWindow::quickSort(int low, int high)
     }
 }
 
+void MainWindow::quickSortTime(int low, int high)
+{
+    if (low < high) {
+        int pi = partitionTime(low, high);
+
+        quickSortTime(low, pi - 1);
+        quickSortTime(pi + 1, high);
+    }
+}
+
+int MainWindow::partitionTime(int low, int high)
+{
+    int pivot = arrayForTime[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arrayForTime[j] < pivot) {
+            i++;
+            std::swap(arrayForTime[i], arrayForTime[j]);
+        }
+    }
+    std::swap(arrayForTime[i + 1], arrayForTime[high]);
+    return (i + 1);
+}
+
 void MainWindow::on_pushButton_clicked()
 {
     if(!isSorted)
@@ -172,20 +311,51 @@ void MainWindow::on_pushButton_clicked()
         ui->pushButton->setDisabled(true);
         ui->lineEdit->setDisabled(true);
 
+        int time;
+
         if(ui->comboBox->currentIndex() == 0)
         {
             isSorted = true;
+
+            timer.start();
+            mergeSortTime(0, m_size - 1);
+            time = timer.nsecsElapsed();
+            ui->label_6->setText(QString::number(time / 1000000000.0, 'f', 9));
+
             mergeSort(0, m_size - 1);
         }
         else if(ui->comboBox->currentIndex() == 1)
         {
             isSorted = true;
+
+            timer.start();
+            quickSortTime(0, m_size - 1);
+            time = timer.nsecsElapsed();
+            ui->label_6->setText(QString::number(time / 1000000000.0, 'f', 9));
+
             quickSort(0, m_size - 1);
         }
         else if(ui->comboBox->currentIndex() == 2)
         {
             isSorted = true;
+
+            timer.start();
+            heapSortTime(m_size);
+            time = timer.nsecsElapsed();
+            ui->label_6->setText(QString::number(time / 1000000000.0, 'f', 9));
+
             heapSort(m_size);
+        }
+        else if(ui->comboBox->currentIndex() == 3)
+        {
+            isSorted = true;
+
+            timer.start();
+            interpolationSortTime();
+            time = timer.nsecsElapsed();
+            ui->label_6->setText(QString::number(time / 1000000000.0, 'f', 9));
+
+            interpolationSort();
         }
 
         ui->pushButton->setEnabled(true);
@@ -215,6 +385,8 @@ void MainWindow::getNumber()
         ui->lineEdit->setReadOnly(true);
     }
     setIndexOfNumber();
+    ui->label_7->setText("Индекс числа в степени " + QString::number(m_size) + " по модулю " + ui->lineEdit->text() + ":");
+    indexInBinpow();
 }
 
 void MainWindow::lineChanged()
@@ -235,7 +407,7 @@ int MainWindow::binarySearch()
         else
             right = mid - 1;
     }
-    return -1;  // если число не найдено
+    return -1;
 }
 
 void MainWindow::setIndexOfNumber()
@@ -245,7 +417,140 @@ void MainWindow::setIndexOfNumber()
         QMessageBox::warning(this, "Ошибка", "Массив должен быть отсортирован");
         return ;
     }
-    int index = binarySearch();
-    ui->label_3->setText(QString::number(index));
+
+    indexOfNumber = binarySearch();
+    ui->label_3->setText(QString::number(indexOfNumber));
+}
+
+//Task 2
+
+void MainWindow::interpolationSort()
+{
+    if (!isSorted) return;
+
+    std::vector<int> divideSize;
+    int end = m_size;
+    divideSize.push_back(end);
+
+    while (!divideSize.empty() && isSorted) {
+        int size = divideSize.back();
+        divideSize.pop_back();
+        int start = end - size;
+        int min = m_data[start];
+        int max = m_data[start];
+
+        for (int i = start + 1; i < end && isSorted; i++) {
+            if (m_data[i] < min) {
+                min = m_data[i];
+            }
+            else if (m_data[i] > max) {
+                max = m_data[i];
+            }
+        }
+
+        if (min == max) {
+            end -= size;
+        }
+        else {
+            int p = 0;
+            std::vector<std::vector<int>> bucket(size);
+            for (int i = start; i < end && isSorted; i++) {
+                p = static_cast<int>(((m_data[i] - min) / static_cast<double>(max - min)) * (size - 1));
+                bucket[p].push_back(m_data[i]);
+            }
+
+            for (int i = 0; i < size && isSorted; i++) {
+                if (!bucket[i].empty()) {
+                    for (int j = 0; j < bucket[i].size() && isSorted; j++) {
+                        m_data[start++] = bucket[i][j];
+
+                        delay(50);  // Задержка в 50 миллисекунд
+                        printArray();
+                        update();
+                    }
+                    divideSize.push_back(bucket[i].size());
+                }
+            }
+        }
+    }
+}
+
+
+void MainWindow::printArray()
+{
+    QString arrayStr;
+    for (int i = 0; i < m_size; ++i) {
+        arrayStr += QString::number(m_data[i]);
+        if (i != m_size - 1) {
+            arrayStr += ", ";
+        }
+    }
+    ui->label_4->setText("Элементы массива: ["+arrayStr+"]");
+}
+
+void MainWindow::interpolationSortTime()
+{
+    if (!isSorted) return;
+
+    std::vector<int> divideSize;
+    int end = m_size;
+    divideSize.push_back(end);
+
+    while (!divideSize.empty() && isSorted) {
+        int size = divideSize.back();
+        divideSize.pop_back();
+        int start = end - size;
+        int min = arrayForTime[start];
+        int max = arrayForTime[start];
+
+        for (int i = start + 1; i < end && isSorted; i++) {
+            if (arrayForTime[i] < min) {
+                min = arrayForTime[i];
+            }
+            else if (arrayForTime[i] > max) {
+                max = arrayForTime[i];
+            }
+        }
+
+        if (min == max) {
+            end -= size;
+        }
+        else {
+            int p = 0;
+            std::vector<std::vector<int>> bucket(size);
+            for (int i = start; i < end && isSorted; i++) {
+                p = static_cast<int>(((arrayForTime[i] - min) / static_cast<double>(max - min)) * (size - 1));
+                bucket[p].push_back(arrayForTime[i]);
+            }
+
+            for (int i = 0; i < size && isSorted; i++) {
+                if (!bucket[i].empty()) {
+                    for (int j = 0; j < bucket[i].size() && isSorted; j++) {
+                        arrayForTime[start++] = bucket[i][j];
+
+                    }
+                    divideSize.push_back(bucket[i].size());
+                }
+            }
+        }
+    }
+}
+
+int MainWindow::binpow(int digit, int powder, int mod)
+{
+    digit %= mod;
+    int result = 1;
+    while (powder > 0) {
+        if (powder & 1)
+            result = (result * digit) % mod;
+        digit = (digit * digit) % mod;
+        powder >>= 1;
+    }
+    return result;
+}
+
+void MainWindow::indexInBinpow()
+{
+    ui->label_8->setText(QString::number(binpow(indexOfNumber, m_size, numberForBinarySearch)));
 }
 
--- Lab4/Task1/mainwindow.h
+++ Lab4/Task1/mainwindow.h
@@ -30,25 +30,43 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
-    int m_size = 400;
+    int m_size = 100;
     int* m_data = nullptr;
-    int* tempData = nullptr;
+    int* arrayForTime = nullptr;
+    QElapsedTimer timer;
     int numberForBinarySearch;
+    int indexOfNumber;
     bool isSorted;
     bool isNumberGood;
 
 
     void merge(int l, int m, int r);
     void mergeSort(int l, int r);
+    void mergeSortTime(int l, int r);
+    void mergeTime(int l, int m, int r);
+
     void heapify(int n, int i);
     void heapSort(int n);
+    void heapSortTime(int n);
+    void heapifyTime(int n, int i);
+
     int partition(int low, int high);
     void quickSort(int low, int high);
-    void delay(int time);
+    void quickSortTime(int low, int high);
+    int partitionTime(int low, int high);
+
     int binarySearch();
     void setIndexOfNumber();
 
+    void interpolationSort();
+    void interpolationSortTime();
 
+    int binpow(int digit, int powder, int mod);
+
+    void indexInBinpow();
+
+    void printArray();
+    void delay(int time);
     void getNumber();
     void lineChanged();
 };
